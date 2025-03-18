aeab8d9eb2f4a9b1852f5ad63138700117f204b3
0x0abc123 <71738504+0x0abc123@users.noreply.github.com>
2025-03-18 18:47:19 +1100

    1742284039 fix comma

--- httprev.py
+++ httprev.py
@@ -211,7 +211,7 @@ def extract_text(s):
 
 
 def build_request(url):
-    return urllib.request.Request(url=url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'} method='GET')
+    return urllib.request.Request(url=url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'}, method='GET')
 
 
 def fetch_favicon_mmh3_hash(url):
